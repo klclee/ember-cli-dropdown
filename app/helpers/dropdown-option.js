174e635d81db81f4ed4e659196248f06f50009b9
klclee <charleetm@gmail.com>
2015-07-22 09:56:04 +0100

    option helper detects weather it can use '.get'

--- app/helpers/dropdown-option.js
+++ app/helpers/dropdown-option.js
@@ -1,7 +1,14 @@
 export default Ember.Handlebars.makeBoundHelper(function(data, valueProp, displayProp) {
-  var value = data[valueProp];
-  var display = data[displayProp];
-
+  var value = '';
+  var display = '';
+  if(typeof data.get === 'function' ){
+    value = data.get(valueProp);
+    display = data.get(displayProp);
+  }else{
+    value = data[valueProp];
+    display = data[displayProp];
+  }
+  
   if(Em.isBlank(valueProp)){
     value = data;
     display = data;
