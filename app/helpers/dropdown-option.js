430f0328730b0d1beb762654cc45ff76f9aa6598
klclee <charleetm@gmail.com>
2015-10-05 15:14:26 +0100

    fixing dropdown for ember 2

--- app/helpers/dropdown-option.js
+++ app/helpers/dropdown-option.js
@@ -1,4 +1,8 @@
-export default Ember.Handlebars.makeBoundHelper(function(data, valueProp, displayProp) {
+// export default Ember.Handlebars.makeBoundHelper(function(data, valueProp, displayProp) {
+export default Ember.Helper.helper(function(params, hash) {
+  var data = params[0];
+  var valueProp = params[1];
+  var displayProp = params[2];
   var value = '';
   var display = '';
   if(typeof data.get === 'function' ){
@@ -8,7 +12,7 @@ export default Ember.Handlebars.makeBoundHelper(function(data, valueProp, displa
     value = data[valueProp];
     display = data[displayProp];
   }
-  
+
   if(Em.isBlank(valueProp)){
     value = data;
     display = data;
