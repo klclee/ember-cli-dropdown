e096f9f0f6d704f896509e6129b4ceddc66dd016
klclee <charleetm@gmail.com>
2016-05-25 17:15:29 +0100

    emeber data and boolean type understands true and false string. Thats not true. This fixes that. If you are using true or false value and need to be binding to a emeber data type filed as real boolean, this fixes that

--- app/components/drop-down.js
+++ app/components/drop-down.js
@@ -3,12 +3,20 @@ export default Em.Component.extend({
   content: null,
   value: '',
   optionValuePath: '',
-  optionLabelPath:'',
-  didInsertElement: function(){
+  optionLabelPath: '',
+  didInsertElement: function() {
     this.$().find(`option[value='${this.value}']`).prop('selected', true);
     var _scope = this;
-    this.$().on('change', function(){
-      _scope.set('value', $("option:selected", this).val());
+    this.$().on('change', function() {
+      var optionValue = false;
+      if ($("option:selected", this).val() === 'true') {
+        optionValue = $("option:selected", this).val() === 'true';
+      } else if ($("option:selected", this).val() === 'false') {
+        optionValue = $("option:selected", this).val() === 'true';
+      } else {
+        optionValue = $("option:selected", this).val();
+      }
+      _scope.set('value', optionValue);
     });
   }
 });
